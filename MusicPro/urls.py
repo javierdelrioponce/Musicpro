fc1bd6813415259d9d29fd8273a4df0a07fbfd0a
Alejandro Cruz <alejandro.cruz.jeria@gmail.com>
2021-07-05 21:39:07 -0400

    Crear app api
    Agregar app api en settings
    Crear archivo serializador de prueba en app api
    Crear viewset de prueba en vista de la app api
    Añadir patron de url api a urls de proyecto MusicPro
    Añadir menu con link de url de la api en el menu principal

--- MusicPro/urls.py
+++ MusicPro/urls.py
@@ -14,14 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-#from django.urls import path, include
+from django.urls import path, include
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.conf.urls.static import static
+from rest_framework import routers
+from api import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
      #path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')), 
     url(r'^admin/', admin.site.urls),
     url(r'', include('blogapp.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
